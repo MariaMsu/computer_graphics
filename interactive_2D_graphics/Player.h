c1d76eae76ae10ebae85c4df3ee1a40cd06ce80d
MariaMsu <i.knaz@yandex.ru>
2021-02-28 23:52:18 +0300

    change code style

--- interactive_2D_graphics/Player.h
+++ interactive_2D_graphics/Player.h
@@ -3,34 +3,31 @@
 
 #include "Image.h"
 
-struct Point
-{
-  int x;
-  int y;
+struct Point {
+    int x;
+    int y;
 };
 
-enum class MovementDir
-{
-  UP,
-  DOWN,
-  LEFT,
-  RIGHT
+enum class MovementDir {
+    UP,
+    DOWN,
+    LEFT,
+    RIGHT
 };
 
-struct Player
-{
-  explicit Player(Point pos = {.x = 10, .y = 10}) :
-                 coords(pos), old_coords(coords) {};
+struct Player {
+    explicit Player(Point pos = {.x = 10, .y = 10}) :
+            coords(pos), old_coords(coords) {};
 
-  bool Moved() const;
-  void ProcessInput(MovementDir dir);
-  void Draw(Image &screen);
+    bool Moved() const;
+    void ProcessInput(MovementDir dir);
+    void Draw(Image &screen);
 
 private:
-  Point coords {.x = 10, .y = 10};
-  Point old_coords {.x = 10, .y = 10};
-  Pixel color {.r = 255, .g = 255, .b = 0, .a = 255};
-  int move_speed = 4;
+    Point coords{.x = 10, .y = 10};
+    Point old_coords{.x = 10, .y = 10};
+    Pixel color{.r = 255, .g = 255, .b = 0, .a = 255};
+    int move_speed = 4;
 
 };
 
