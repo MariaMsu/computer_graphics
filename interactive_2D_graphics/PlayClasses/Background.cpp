f1e6b1186e0281bc901e1fc8a9f8bd9612c2bd49
MariaMsu <i.knaz@yandex.ru>
2021-03-04 15:06:32 +0300

    add lava

--- interactive_2D_graphics/PlayClasses/Background.cpp
+++ interactive_2D_graphics/PlayClasses/Background.cpp
@@ -6,19 +6,17 @@ std::map<int, std::shared_ptr<Image>> readBackgroundTitles(const std::string &ti
     std::map<int, std::shared_ptr<Image>> titles_map;
     std::ifstream title_file(titles_path);
     if (!title_file.is_open()) {
-        std::cout << "Unable to open title's path file " << titles_path << "\n";
+        std::cerr << "Unable to open title's path file " << titles_path << "\n";
         exit(4);
     }
-    int i = 0;  // todo
-    while (std::getline(title_file, path)) {
-        if (path.length() != 0){
+    for (int i = 0; std::getline(title_file, path); ++i) {
+        if (path.length() != 0) {
             std::pair<int, std::shared_ptr<Image>> new_item =
                     std::pair<int, std::shared_ptr<Image>>(i, std::make_shared<Image>(path));
             titles_map.insert(new_item);
         }
-        ++i;
     }
-    std::cout<<"\n";
+    std::cout << "\n";
     title_file.close();
     return titles_map;
 };
@@ -41,8 +39,8 @@ void Background::DrawRoom(Image &screen, GlobalState &screen_state) {
     for (int y = 0; y < h_WINDOW_T_HEIGHT; ++y) {
         for (int x = 0; x < h_WINDOW_T_WIDTH; ++x) {
             int key = background_map[y][x];
-            if (titles_map.count(key) == 0){
-                std::cerr<<"Title "<<key<<" did not loaded\n";
+            if (titles_map.count(key) == 0) {
+                std::cerr << "Title " << key << " did not loaded\n";
                 exit(5);
             }
             const std::shared_ptr<Image> &title = this->titles_map[background_map[y][x]];
